0fb600cc3c2fd348cfba2d6c9582c970ee1692f4
AgaspherGames <agasphergg@gmail.com>
2023-11-02 22:10:16 +0600

    fixed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CreateSection } from "@/components/component/create-section";
 import { Features } from "@/components/component/features";
 import { Header } from "@/components/component/header";
 import { Hero } from "@/components/component/hero";
@@ -5,9 +6,12 @@ import { Landing } from "@/components/component/landing";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="md:text-lg">
-    <Header/>
-    <Hero/>
-    <Features/>
-  </main>;
+  return (
+    <main className="bg-black min-h-screen md:text-lg">
+      <Header />
+      <Hero />
+      <Features />
+      <CreateSection />
+    </main>
+  );
 }
--- components/component/features.tsx
+++ components/component/features.tsx
@@ -3,58 +3,36 @@
  * @see https://v0.dev/t/HVHcTMTJiYU
  */
 
-
 export function Features() {
   return (
-    <section className="w-full py-8 md:py-12 lg:py-24 xl:py-32 bg-black text-white">
-      <div className="container mx-auto px-4 md:px-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+    <section className="w-full py-8 md:py-12 lg:py-24 xl:py-32 bg-black text-white text-base lg:text-xl">
+      <div className="container mx-auto px-2 md:px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6 md:gap-8">
           <div className="flex flex-col items-center space-y-4 text-center">
             <svg
-              className=" h-10 w-10 text-cyan-500"
+              className=" h-10 w-10 lg:h-16 lg:w-16 text-cyan-500"
               fill="none"
-              height="24"
               stroke="currentColor"
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
               viewBox="0 0 24 24"
-              width="24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
               <path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z" />
             </svg>
-            <h3 className="text-lg font-semibold">Интерактивные викторины</h3>
-            <p className="mx-auto max-w-[200px] text-zinc-200">
-              Задействуйте свой мозг с помощью сложных викторин и улучшите свои знания.
+            <h3 className="text-xl md:text-xl lg:text-3xl font-semibold">
+              Интерактивные викторины
+            </h3>
+            <p className="mx-auto max-w-[200px] lg:max-w-[400px] text-zinc-200">
+              Задействуйте свой мозг с помощью сложных викторин и улучшите свои
+              знания.
             </p>
           </div>
           <div className="flex flex-col items-center space-y-4 text-center">
             <svg
-              className=" h-10 w-10 text-cyan-500"
-              fill="none"
-              height="24"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              width="24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line x1="10" x2="14" y1="2" y2="2" />
-              <line x1="12" x2="15" y1="14" y2="11" />
-              <circle cx="12" cy="14" r="8" />
-            </svg>
-            <h3 className="text-lg font-semibold">Таймированные раунды</h3>
-            <p className="mx-auto max-w-[200px] text-zinc-200">
-              Проверьте свою скорость и точность в таймированных раундах викторины.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-4 text-center">
-            <svg
-              className=" h-10 w-10 text-cyan-500"
+              className=" h-10 w-10 lg:h-16 lg:w-16 text-cyan-500"
               fill="none"
               height="24"
               stroke="currentColor"
@@ -72,13 +50,40 @@ export function Features() {
               <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
               <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
             </svg>
-            <h3 className="text-lg font-semibold">Лидеры</h3>
-            <p className="mx-auto max-w-[200px] text-zinc-200">
+            <h3 className="text-xl md:text-xl lg:text-3xl font-semibold">
+              Списки лидеров
+            </h3>
+            <p className="mx-auto max-w-[200px] lg:max-w-[400px] text-zinc-200 ">
               Соревнуйтесь с друзьями и поднимайтесь по лидерской доске.
+            </p>
+          </div>
+          <div className="flex flex-col items-center space-y-4 text-center sm:col-span-2 lg:col-span-1">
+            <svg
+              className=" h-10 w-10 lg:h-16 lg:w-16 text-cyan-500"
+              fill="none"
+              height="24"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              width="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line x1="10" x2="14" y1="2" y2="2" />
+              <line x1="12" x2="15" y1="14" y2="11" />
+              <circle cx="12" cy="14" r="8" />
+            </svg>
+            <h3 className="text-xl md:text-xl lg:text-3xl font-semibold">
+              Таймированные раунды
+            </h3>
+            <p className="mx-auto max-w-[200px] lg:max-w-[400px] text-zinc-200">
+              Проверьте свою скорость и точность в таймированных раундах
+              викторины.
             </p>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
