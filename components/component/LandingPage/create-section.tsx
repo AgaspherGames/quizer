b591f0b7cff69f98c3e034450716ca9952dcb160
AgaspherGames <agasphergg@gmail.com>
2023-12-01 13:14:25 +0600

    updated landing page

--- components/component/LandingPage/create-section.tsx
+++ components/component/LandingPage/create-section.tsx
@@ -4,29 +4,39 @@
  */
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Card3D } from "../Base/Card3D";
 
 export function CreateSection() {
   return (
-    <section className="w-full bg-black text-white py-8 md:py-16 lg:py-24">
-      <div className="container mx-auto px-4 md:px-8 lg:px-12">
-        <h2 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl text-cyan-400">
-          Создай свою собственную викторину
-        </h2>
-        <p className="mt-4 text-center text-base lg:text-xl">
-          Преобразуйте свои знания и идеи в увлекательную и захватывающую
-          викторину для проверки знаний ваших друзей и семьи.
-        </p>
-        <div className="mt-8 flex justify-center">
-          <Link href={"/quiz/create"}>
-            <Button
-              className="px-6 py-4 text-base lg:py-6 lg:px-8 lg:text-xl bg-white text-black"
-              variant="secondary"
-            >
-              Начать создание
-            </Button>
-          </Link>
+    <>
+      <section className="w-full px-2 py-8 md:py-12 lg:py-24 ">
+        <div className="relative w-full py-3 sm:max-w-3xl sm:mx-auto">
+          <Card3D>
+            <div className="p-8 xl:p-14 bg-zinc-950 shadow-xl shadow-zinc-900 rounded-3xl hover:scale-105 transition-all">
+              <div className="space-y-8 text-center">
+                <h1 className="text-2xl font-bold tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-4xl/none">
+                  Создай свою собственную викторину
+                </h1>
+                <p className="mx-auto max-w-[700px] text-cyan-500 text-lg md:text-xl dark:text-cyan-400">
+                  Преобразуйте свои знания и идеи в увлекательную и
+                  захватывающую викторину для проверки знаний ваших друзей и
+                  семьи.
+                </p>
+                <div className="space-x-4">
+                  <Link href={"/quiz/create"}>
+                    <Button
+                      className="px-6 py-4 text-lg lg:py-6 lg:px-8 lg:text-xl bg-white text-black"
+                      variant="secondary"
+                    >
+                      Начать создание
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Card3D>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
